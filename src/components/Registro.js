77091d5976cf8d54cb56917f7866aee11993d5e9
Martin Ayo <mrtnayo@gmail.com>
2021-02-01 23:37:27 -0500

    REfactoriza vista chain.

--- src/components/Registro.js
+++ src/components/Registro.js
@@ -1,100 +1,100 @@
-import React, {useState,} from 'react';
+import React, { useState, } from 'react';
 import { useHistory } from 'react-router-dom';
 
 // const url = process.env.REACT_APP_LOGIN; //api real
-const url='http://localhost:3000/transaccion';
+const url = 'http://localhost:5000/transaccion';
 
-const  Registro= () => {
+const Registro = () => {
 
- const history= useHistory();
-    const[formData, setFormData]=useState({
-        sender:'',
-        recipient:'',
-        code:''
-    });
+  const history = useHistory();
+  const [formData, setFormData] = useState({
+    sender: '',
+    recipient: '',
+    code: ''
+  });
 
 
-    const registrar=async (e)=>{
-        e.preventDefault();
-     
-        try {
-        
-              const res = await fetch(url,{
-              method: 'POST',
-              body: JSON.stringify(formData),
-              headers: {
-                // 'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              }
-            })
-            const formatoJson = await res.json();
-            console.log(formatoJson);
-            
-          
-            setFormData({
-                sender:'',
-                recipient:'',
-                code:''
-              })  
-              history.push("/chain");
-          
-          } catch (error) {
-            console.log(error.message);
-          }
-        
+  const registrar = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const res = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          // 'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      })
+      const formatoJson = await res.json();
+      console.log(formatoJson);
+
+
+      setFormData({
+        sender: '',
+        recipient: '',
+        code: ''
+      })
+      history.push("/chain");
+
+    } catch (error) {
+      console.log(error.message);
     }
 
-    const handleChange =  (e) => {
-       
-      setFormData({
-          ...formData,
-          [e.target.name]: e.target.value
-        })    
-      }
+  }
 
-    return(
+  const handleChange = (e) => {
 
-        <div className="Container">
-        <div className="row">
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  return (
+
+    <div className="Container">
+      <div className="row">
         <div className="col-md-5 mx-auto ">
-        
-         <div className="container register"> 
-        <form onSubmit={registrar}>
-            <label className="title">Registro</label>
+
+          <div className="container register">
+            <form onSubmit={registrar}>
+              <label className="title">Registro</label>
 
 
-            <div className="form-group col">
-              <label >Emisor</label>
-              <input type="text" className="form-control"  placeholder="Ingresa DNI" 
-              value={formData.sender} onChange={handleChange} name="sender"/>
+              <div className="form-group col">
+                <label >Emisor</label>
+                <input type="text" className="form-control" placeholder="Ingresa DNI"
+                  value={formData.sender} onChange={handleChange} name="sender" />
               </div>
-            <div className="form-group col">
-            <label >Receptor</label>
-            <input type="text" className="form-control" placeholder="Ingresa DNI" 
-            value={formData.recipient} onChange={handleChange} name="recipient"/>
-            </div>
-            <div className="form-group col">
-            <label >Botella</label>
-            <input type="text" className="form-control"  placeholder="Ingresa código de la botella" 
-            value={formData.code} onChange={handleChange} name="code"/>
-            </div>
-           
-        
-    
-            <div className="form-group col">    
-            <button type="submit" className="btn btn-success  btn-block"
-            // onClick={()=>history.push("/chain")}
-            >Registrar</button>
-            </div>
-        
-        </form>
-</div>
-</div>
-</div>
-</div>
-    );
+              <div className="form-group col">
+                <label >Receptor</label>
+                <input type="text" className="form-control" placeholder="Ingresa DNI"
+                  value={formData.recipient} onChange={handleChange} name="recipient" />
+              </div>
+              <div className="form-group col">
+                <label >Botella</label>
+                <input type="text" className="form-control" placeholder="Ingresa código de la botella"
+                  value={formData.code} onChange={handleChange} name="code" />
+              </div>
+
+
+
+              <div className="form-group col">
+                <button type="submit" className="btn btn-success  btn-block"
+                // onClick={()=>history.push("/chain")}
+                >Registrar</button>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
- 
+
 
 
 export default Registro;
--- src/components/chain.js
+++ src/components/chain.js
@@ -1,64 +1,73 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 
-const url="http://localhost:3000/transaccion";
+const url = "http://localhost:5000/chain";
 
 const Chain = () => {
 
-    const [cadena, setCadena]=useState([]);
+    const [cadena, setCadena] = useState([]);
 
-const getData= async()=>{
+    const getData = async () => {
 
-   try {
-       const res= await fetch(url);
-       const data=await res.json();
-       console.log(data);
-       setCadena(data);
-       
-   } catch (error) {
-       console.log(error.message);
-   }
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+            setCadena(data.chain);
 
-}
-
-     useEffect( () => {
-       getData();
+        } catch (error) {
+            console.log(error.message);
         }
-    ,[])
+
+    }
+
+    useEffect(() => {
+        getData();
+    }
+        , [])
 
     return (
-            <div className="container mt-4 pt-4">
+        <div className="container mt-4 pt-4">
 
-                <div className="row ">
+            <div className="row ">
                 <div className="col-6 offset-3">
-                
-                <table className="table mt-4">
-                <thead className="thead-dark">
-                    <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Descripción</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        cadena.map((item)=>{
-                           return(
-                            <tr key={item.id}>
-                            <th scope="row">{item.id}</th>
-                            <td>{item.sender}</td>
-                            
+
+                    <table className="table mt-4">
+                        <thead className="thead-dark">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Descripción</th>
                             </tr>
-                           ) 
-                        })
-                    }
-                   
-                    
-                </tbody>
-                </table>
-                </div>
+                        </thead>
+                        <tbody>
+                            {
+                                cadena.map((item) => {
+                                    return (
+                                        <tr key={item.index}>
+                                            <th scope="row">{item.index}</th>
+                                            <td>{item.transactions.map((t, index) =>
+                                            (
+                                                <ul key={index}>
+                                                    <li>código: {t.code}</li>
+                                                    <li>emisor :{t.sender}</li>
+                                                    <li>receptor{t.recipient}</li>
+                                                </ul>
+                                            ))
+
+                                            }</td>
+
+                                        </tr>
+                                    )
+                                })
+                            }
+
+
+                        </tbody>
+                    </table>
                 </div>
             </div>
-      );
+        </div>
+    );
 }
- 
+
 export default Chain;
